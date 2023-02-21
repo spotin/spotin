d8760f8de15cc73fdd3401faa025b474409eacc8
Vincent Guidoux <vincent.guidoux@gmail.com>
2023-02-21 10:40:34 +0000

    refactor

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
