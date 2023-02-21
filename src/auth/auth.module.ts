f66cd1058068b18d4d1f8f009bc59bc9b36da5b8
Vincent Guidoux <vincent.guidoux@gmail.com>
2023-02-21 16:24:25 +0000

    update expiration for JWT token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt/jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '600s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
