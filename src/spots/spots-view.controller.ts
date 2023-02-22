0e9967d2970ff42cbebb10ee9f80ec140981ab17
Vincent Guidoux <vincent.guidoux@gmail.com>
2023-02-22 08:29:35 +0000

    create a page to view a spot

--- src/spots/spots-view.controller.ts
+++ src/spots/spots-view.controller.ts
@@ -1,5 +1,14 @@
-import { Get, Controller, Render, Post, UseGuards } from '@nestjs/common';
+import {
+  Get,
+  Controller,
+  Render,
+  Post,
+  UseGuards,
+  Param,
+  UseFilters,
+} from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth-guards';
+import { UnauthorizedExceptionFilter } from 'src/filters/unauthorized-exception.filter';
 import { SpotsService } from './spots.service';
 
 @Controller('spots')
@@ -8,9 +17,19 @@ export class SpotsViewsController {
 
   @UseGuards(JwtAuthGuard)
   @Get('list')
+  @UseFilters(new UnauthorizedExceptionFilter())
   @Render('spots/list')
   async root() {
     const spots = await this.spotsService.getSpots();
     return { spots };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':uuid')
+  @UseFilters(new UnauthorizedExceptionFilter())
+  @Render('spots/[uuid]')
+  async getSpot(@Param('uuid') uuid: string) {
+    const [spot] = await this.spotsService.getSpot(uuid);
+    return { spot };
   }
 }
