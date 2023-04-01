4ba775151f87b522d36331c6b5346d12d5f1d438
Vincent Guidoux <vincent.guidoux@gmail.com>
2023-04-01 22:09:22 +0000

    enhance openapi documentation

--- src/users/users-api.controller.ts
+++ src/users/users-api.controller.ts
@@ -10,7 +10,14 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserDto } from './dto/user.dto';
 
 @ApiTags('Users')
 @Controller('api/users')
@@ -23,26 +30,92 @@ export class UsersApiController {
     description: 'Create a user.',
     operationId: 'createUserApi',
   })
+  @ApiOkResponse({
+    description: 'User has been successfully created.',
+    type: CreateUserDto,
+  })
   createUserApi(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all users.',
+    description: 'Get all users.',
+    operationId: 'findAllUsersApi',
+  })
+  @ApiOkResponse({
+    description: 'Users have been successfully retrieved.',
+    type: [UserDto],
+  })
+  @ApiNotFoundResponse({
+    description: 'No users found.',
+  })
   findAllUsersApi() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get a user.',
+    description: 'Get a user.',
+    operationId: 'findOneUserApi',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The id of the user.',
+    type: String,
+  })
+  @ApiOkResponse({
+    description: 'User has been successfully retrieved.',
+    type: UserDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
   findOneUserApi(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update a user.',
+    description: 'Update a user.',
+    operationId: 'updateUserApi',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The id of the user.',
+    type: String,
+  })
+  @ApiOkResponse({
+    description: 'User has been successfully updated.',
+    type: UpdateUserDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
   updateUserApi(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete a user.',
+    description: 'Delete a user.',
+    operationId: 'removeUserApi',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'The id of the user.',
+    type: String,
+  })
+  @ApiOkResponse({
+    description: 'User has been successfully deleted.',
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found.',
+  })
   removeUserApi(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
