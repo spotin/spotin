6e0ef83aa5ae302c52f8cb269fdd874518a9b289
Vincent Guidoux <vincent.guidoux@gmail.com>
2023-04-25 11:35:47 +0200

    change token strategy name

--- src/auth/hybrid/hybrid-auth.guard.ts
+++ src/auth/hybrid/hybrid-auth.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class HybridAuthGuard extends AuthGuard(['jwt', 'apiKey']) {}
+export class HybridAuthGuard extends AuthGuard(['jwt', 'token']) {}
