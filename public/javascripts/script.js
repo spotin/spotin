f5e56e99c0e22517318817f658ccfe4aebd44b20
Vincent Guidoux <vincent.guidoux@gmail.com>
2023-04-25 18:49:20 +0200

    add map

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -19,26 +19,6 @@
     },
   });
 
-  // Display the maps
-  document.querySelectorAll('.map').forEach((elem) => {
-    // Create the map
-    const map = new maplibregl.Map({
-      container: elem,
-      style:
-        'https://vectortiles.geo.admin.ch/styles/ch.swisstopo.leichte-basiskarte_world.vt/style.json',
-      center: [elem.dataset.longitude, elem.dataset.latitude],
-      zoom: 9, // starting zoom
-    });
-
-    // Add the marker
-    new maplibregl.Marker({
-      color: '#e53935',
-      //draggable: true,
-    })
-      .setLngLat([elem.dataset.longitude, elem.dataset.latitude])
-      .addTo(map);
-  });
-
   // Convert UTC time to local time in time elements
   document.querySelectorAll('time.datetime').forEach((elem) => {
     let value = elem.textContent;
