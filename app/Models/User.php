b46ba05abe3ad6077710314e8377848917075355
Vincent <vincent.guidoux@gmail.com>
2026-07-07 13:18:03 +0000

    feat: integrate Laravel Sanctum for API authentication and add personal access tokens migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,13 +11,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['username', 'email', 'password', 'bio', 'role', 'enabled'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, HasUuids, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, HasUuids, Notifiable, SoftDeletes;
 
     protected function casts(): array
     {
