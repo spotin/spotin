f426fd95984b98a9edc3e7349dae025c5215d92c
Nortalle <vincent.guidoux@gmail.com>
2026-08-04 12:56:06 +0000

    Add prettier-plugin-blade for Blade file formatting and update welcome page structure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Welcome</title>
+	@vite (['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Welcome to My Website</h1>
+	<h1>Welcome to My Website</h1>
 
-<button class="btn">Button</button>
+	<button class="btn">Button</button>
 </body>
 </html>
