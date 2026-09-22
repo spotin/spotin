19c4fc3d6a5eee098265579e75e56dc49b27a3b1
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-09-22 14:48:03 +0000

    Create default layout

---
+++ app/View/Components/DefaultLayout.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class DefaultLayout extends Component
+{
+	/**
+	 * Create a new component instance.
+	 */
+	public function __construct(
+		public ?string $title,
+		public ?string $description,
+	) {
+		//
+	}
+
+	/**
+	 * Get the view / contents that represent the component.
+	 */
+	public function render(): View|Closure|string
+	{
+		return view("components.default-layout");
+	}
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,16 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>{{ config('app.name') }}</title>
-		@vite(['resources/css/app.css', 'resources/js/app.js'])
-	</head>
-	<body>
-		<article class="prose lg:prose-xl">
-			<h1>{{ __('ui.welcome.title') }}</h1>
+<x-default-layout
+	:title="__('ui.welcome.title')"
+	:description="__('ui.welcome.description')">
+	<article class="prose lg:prose-xl">
+		<h1>{{ __('ui.welcome.title') }}</h1>
 
-			<p>{{ __('ui.welcome.first_paragraph') }}</p>
+		<p>{{ __('ui.welcome.first_paragraph') }}</p>
 
-			<p>{{ __('ui.welcome.second_paragraph') }}</p>
+		<p>{{ __('ui.welcome.second_paragraph') }}</p>
+
+		<div>
 			<button class="btn">{{ __('ui.welcome.button') }}</button>
-			<form method="POST" action="{{ route('locale.update') }}">
-				@csrf
-				@method('PATCH')
-				<select
-					id="locale"
-					name="locale"
-					onchange="this.form.submit()"
-					class="select">
-					<option value="en" @selected(app()->getLocale() === 'en')>English</option>
-					<option value="fr" @selected(app()->getLocale() === 'fr')>Français</option>
-				</select>
-			</form>
-		</article>
-	</body>
-</html>
+		</div>
+	</article>
+</x-default-layout>
 
