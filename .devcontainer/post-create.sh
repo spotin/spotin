764f303b340a53d84fca8642e039745cb5fe52e6
Vincent Guidoux <vincent.guidoux@gmail.com>
2026-09-22 11:03:54 +0200

    Setup Laravel (#262)
    Co-authored-by: Ludovic Delafontaine <ludovic.delafontaine@gmail.com>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,6 +4,8 @@
 # Aliases for common commands
 tee -a ~/.bash_aliases > /dev/null <<"EOF"
 alias tree='tree --dirsfirst -A -F'
+alias jpegoptim='jpegoptim --strip-all --all-progressive'
+alias optipng='optipng -o5 -strip all -fix'
 EOF
 
 ## Install required packages
@@ -15,6 +17,20 @@ sudo apt install --yes neovim
 
 # Install network utilities (dig, nslookup, ping, ncat)
 sudo apt install --yes dnsutils iputils-ping ncat
+
+# Install packages to interact with SQLite databases
+sudo apt install --yes sqlite3 php-sqlite3
+
+# Install PHP and Composer
+sudo apt install --yes php-common php-cli php-mysql php-xml php-bcmath composer
+
+## Install Laravel Installer
+composer global require laravel/installer
+
+# Add Composer global bin to PATH
+tee -a ~/.bashrc > /dev/null <<"EOF"
+export PATH="${HOME}/.composer/vendor/bin:${PATH}"
+EOF
 
 ## Git configuration
 # Set pull to rebase by default
