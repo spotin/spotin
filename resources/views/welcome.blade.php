dfe24c3619358359f1b244f8400e23090642270e
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-09-22 12:26:34 +0000

    Finalize Tailwind/daisyUI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-<article class="prose lg:prose-xl">
-  <h1>Garlic bread with cheese: What the science tells us</h1>
-  <p>
-    For years parents have espoused the health benefits of eating garlic bread with cheese to their
-    children, with the food earning such an iconic status in our culture that kids will often dress
-    up as warm, cheesy loaf for Halloween.
-  </p>
-  <p>
-    But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-    springing up around the country.
-  </p>
-  <!-- ... -->
-</article>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Welcome</title>
+		@vite(['resources/css/app.css', 'resources/js/app.js'])
+	</head>
+	<body>
+		<article class="prose lg:prose-xl">
+			<h1>Heading text</h1>
 
-<button class="btn">Button</button>
-</body>
+			<p>
+				First paragraph text.
+			</p>
+
+			<p>
+				Second paragraph text.
+			</p>
+			<button class="btn">Button</button>
+		</article>
+	</body>
 </html>
+
