9f5cf50bda85c35bfa37e3a5dc5600fea051c97c
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2026-09-22 12:57:33 +0000

    Add language files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Welcome</title>
+		<title>{{ config('app.name') }}</title>
 		@vite(['resources/css/app.css', 'resources/js/app.js'])
 	</head>
 	<body>
 		<article class="prose lg:prose-xl">
-			<h1>Heading text</h1>
+			<h1>{{ __('ui.welcome') }}</h1>
 
 			<p>
 				First paragraph text.
